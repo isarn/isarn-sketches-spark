00a7c3f94ec9f601b0bfefcde1fef4d37a1f04ca
Erik Erlandson <eje@redhat.com>
2017-07-04 12:04:59 -0700

    documentation for 0.1.0 (#2)
    * scaladoc
    
    * fix bug in TDigestArrayUDAF, forgot to re-store updated buffer
    
    * version 0.1.0
    
    * t-digest UDAF examples

--- src/main/scala/org/isarnproject/sketches/udaf/package.scala
+++ src/main/scala/org/isarnproject/sketches/udaf/package.scala
@@ -20,25 +20,91 @@ import org.apache.spark.sql.catalyst.util._
 
 import org.apache.spark.isarnproject.sketches.udt._
 
+/** package-wide methods, implicits and definitions for sketching UDAFs */
 package object udaf {
+  /**
+   * Obtain a UDAF for sketching a single numeric Dataset column using a t-digest
+   * @tparam N The numeric type of the column; Double, Int, etc
+   * @return A UDAF that can be applied to a Dataset column
+   * @example
+   * {{{
+   * import org.isarnproject.sketches.udaf._, org.apache.spark.isarnproject.sketches.udt._
+   * // create a UDAF for a t-digest, adding custom settings for delta and maxDiscrete
+   * val udafTD = tdigestUDAF[Double].delta(0.1).maxDiscrete(25)
+   * // apply the UDAF to get a t-digest for a data column
+   * val agg = data.agg(udafTD($"NumericColumn"))
+   * // extract the t-digest
+   * val td = agg.getAs[TDigestSQL](0).tdigest
+   * }}}
+   */
   def tdigestUDAF[N](implicit
     num: Numeric[N],
     dataType: TDigestUDAFDataType[N])
   = TDigestUDAF(TDigest.deltaDefault, 0)
 
+  /**
+   * Obtain a UDAF for sketching a numeric array-data Dataset column, using a t-digest for
+   * each element.
+   * @tparam N The numeric type of the array-data column; Double, Int, etc
+   * @return A UDAF that can be applied to a Dataset array-data column
+   * @example
+   * {{{
+   * import org.isarnproject.sketches.udaf._, org.apache.spark.isarnproject.sketches.udt._
+   * // create a UDAF for t-digest array, adding custom settings for delta and maxDiscrete
+   * val udafTD = tdigestArrayUDAF[Double].delta(0.1).maxDiscrete(25)
+   * // apply the UDAF to get an array of t-digests for each element in the array-data
+   * val agg = data.agg(udafTD($"NumericArrayColumn"))
+   * // extract the t-digest array
+   * val tdArray = agg.getAs[TDigestArraySQL](0).tdigests
+   * }}}
+   */
   def tdigestArrayUDAF[N](implicit
     num: Numeric[N],
     dataType: TDigestUDAFDataType[N])
   = TDigestArrayUDAF(TDigest.deltaDefault, 0)
 
+  /**
+   * Obtain a UDAF for sketching an ML Vector Dataset column, using a t-digest for
+   * each element in the vector
+   * @return A UDAF that can be applied to a ML Vector column
+   * @example
+   * {{{
+   * import org.isarnproject.sketches.udaf._, org.apache.spark.isarnproject.sketches.udt._
+   * // create a UDAF for t-digest array, adding custom settings for delta and maxDiscrete
+   * val udafTD = tdigestMLVecUDAF[Double].delta(0.1).maxDiscrete(25)
+   * // apply the UDAF to get an array of t-digests for each element in the array-data
+   * val agg = data.agg(udafTD($"MLVecColumn"))
+   * // extract the t-digest array
+   * val tdArray = agg.getAs[TDigestArraySQL](0).tdigests
+   * }}}
+   */
   def tdigestMLVecUDAF = TDigestMLVecUDAF(TDigest.deltaDefault, 0)
 
+  /**
+   * Obtain a UDAF for sketching an MLLib Vector Dataset column, using a t-digest for
+   * each element in the vector
+   * @return A UDAF that can be applied to a MLLib Vector column
+   * @example
+   * {{{
+   * import org.isarnproject.sketches.udaf._, org.apache.spark.isarnproject.sketches.udt._
+   * // create a UDAF for t-digest array, adding custom settings for delta and maxDiscrete
+   * val udafTD = tdigestMLLibVecUDAF[Double].delta(0.1).maxDiscrete(25)
+   * // apply the UDAF to get an array of t-digests for each element in the array-data
+   * val agg = data.agg(udafTD($"MLLibVecColumn"))
+   * // extract the t-digest array
+   * val tdArray = agg.getAs[TDigestArraySQL](0).tdigests
+   * }}}
+   */
   def tdigestMLLibVecUDAF = TDigestMLLibVecUDAF(TDigest.deltaDefault, 0)
 
+  /** implicitly unpack a TDigestSQL to extract its TDigest payload */
   implicit def implicitTDigestSQLToTDigest(tdsql: TDigestSQL): TDigest = tdsql.tdigest
+
+  /** implicitly unpack a TDigestArraySQL to extract its Array[TDigest] payload */
   implicit def implicitTDigestArraySQLToTDigestArray(tdasql: TDigestArraySQL): Array[TDigest] =
     tdasql.tdigests
 
+  /** For declaring implicit values that map numeric types to corresponding DataType values */
   case class TDigestUDAFDataType[N](tpe: DataType)
 
   implicit val tDigestUDAFDataTypeByte = TDigestUDAFDataType[Byte](ByteType)
