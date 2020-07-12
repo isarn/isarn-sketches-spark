10dbee2c50c7503fac386f1529f66f7c5d0ccf2c
Erik Erlandson <eerlands@redhat.com>
2020-07-12 07:38:07 -0700

    explicit import of JavaPredictionModel

--- python/isarnproject/pipelines/fi.py
+++ python/isarnproject/pipelines/fi.py
@@ -1,7 +1,7 @@
 from pyspark import since, keyword_only
 from pyspark.ml.param.shared import *
 from pyspark.ml.util import *
-from pyspark.ml.wrapper import JavaEstimator, JavaModel, JavaWrapper
+from pyspark.ml.wrapper import JavaEstimator, JavaModel, JavaWrapper, JavaPredictionModel
 from pyspark.ml.common import inherit_doc
 from pyspark.sql import DataFrame
 
