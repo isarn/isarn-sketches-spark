3db403c3e4a767ebe6759a33b431375601c9ea79
Erik Erlandson <eerlands@redhat.com>
2020-07-13 11:30:08 -0700

    fix package object path

--- src/main/scala/org/isarnproject/pipelines/spark/package.scala
+++ src/main/scala/org/isarnproject/pipelines/spark/package.scala
@@ -11,11 +11,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package org.isarnproject
+package org.isarnproject.pipelines
 
 import scala.language.existentials
 
-package object pipelines {
+package object spark {
   private[pipelines] def inheritances(obj: Any): Seq[String] = {
     var ih: List[Class[_]] = Nil
     var sc = obj.getClass.getSuperclass
