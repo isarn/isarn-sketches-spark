ae67bb9b99b8723250b919e289f8adb07a9190fe
Erik Erlandson <eerlands@redhat.com>
2020-07-19 16:18:10 -0700

    TDigestAggregator test

--- src/test/scala/org/isarnproject/sketches/spark/tdigest.scala
+++ src/test/scala/org/isarnproject/sketches/spark/tdigest.scala
@@ -13,14 +13,37 @@ limitations under the License.
 
 package org.isarnproject.sketches.spark.tdigest
 
-import org.isarnproject.testing.spark.SparkTestSuite
+import scala.util.Random._
+
 import utest._
 
-object TDigestTestSuite extends SparkTestSuite {
+import org.isarnproject.testing.spark.SparkTestSuite
+import org.apache.spark.sql.functions._
+
+import org.isarnproject.sketches.java.TDigest
+
+object TDigestAggregationSuite extends SparkTestSuite {
+
+  // set the seed before generating any data
+  setSeed(7337L * 3773L)
+  // don't use lazy values because then data generation order may be undefined,
+  // due to test execution order
+  val data1 = spark.createDataFrame(Vector.fill(10000){(nextInt(10), nextGaussian)})
+    .toDF("j","x")
+    .cache()
+
   val tests = Tests {
-    test("smoke") {
-      val t = spark
+    test("TDigestAggregator") {
+      assert(data1.rdd.partitions.size > 1)
+      val udf = TDigestAggregator.udf[Double](compression = 0.2, maxDiscrete = 25)
+      val agg = data1.agg(udf(col("j")), udf(col("x"))).first
+      val (tdj, tdx) = (agg.getAs[TDigest](0), agg.getAs[TDigest](1))
+      approx(tdj.cdf(2), 0.20, eps = 0.05)
+      approx(tdx.cdf(2), 0.97, eps = 0.05)
     }
   }
+
+  def approx(x: Double, t: Double, eps: Double = 1e-4): Unit =
+    assert(math.abs(x - t) < eps)
 }
 
