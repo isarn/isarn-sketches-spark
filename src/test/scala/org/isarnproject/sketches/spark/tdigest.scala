864076c3763e39f39778935bccb769dfe8a5cd44
Erik Erlandson <eerlands@redhat.com>
2020-07-20 16:13:13 -0700

    KSD cumulative distribution divergence measure for unit testing

--- src/test/scala/org/isarnproject/sketches/spark/tdigest.scala
+++ src/test/scala/org/isarnproject/sketches/spark/tdigest.scala
@@ -24,16 +24,23 @@ import org.isarnproject.sketches.java.TDigest
 
 object TDigestAggregationSuite extends SparkTestSuite {
 
+  import CDFFunctions._
+
   // set the seed before generating any data
   setSeed(7337L * 3773L)
+
   // don't use lazy values because then data generation order may be undefined,
   // due to test execution order
   val data1 = spark.createDataFrame(Vector.fill(10000){(nextInt(10), nextGaussian)})
     .toDF("j","x")
     .cache()
 
-  // make sure data are actually created prior to testing
+  // Spark DataFrames and RDDs are lazy.
+  // Make sure data are actually created prior to testing, or ordering
+  // may change based on test ordering
   data1.count()
+
+  val epsD = 0.01
 
   val tests = Tests {
     test("TDigestAggregator") {
@@ -41,8 +48,8 @@ object TDigestAggregationSuite extends SparkTestSuite {
       val udf = TDigestAggregator.udf[Double](compression = 0.2, maxDiscrete = 25)
       val agg = data1.agg(udf(col("j")), udf(col("x"))).first
       val (tdj, tdx) = (agg.getAs[TDigest](0), agg.getAs[TDigest](1))
-      approx(tdj.cdf(2), 0.20, eps = 0.05)
-      approx(tdx.cdf(2), 0.97, eps = 0.05)
+      assert(KSD(tdj, discreteUniformCDF(0, 9)) < epsD)
+      assert(KSD(tdx, gaussianCDF(0,1)) < epsD)
     }
   }
 
@@ -50,3 +57,51 @@ object TDigestAggregationSuite extends SparkTestSuite {
     assert(math.abs(x - t) < eps)
 }
 
+object CDFFunctions {
+  type CDF = Double => Double
+
+  def KSD(td: TDigest, cdf: CDF, n: Int = 1000): Double = {
+    require(td.size() > 1)
+    require(n > 0)
+    val xmin = td.cdfInverse(0)
+    val xmax = td.cdfInverse(1)
+    val step = (xmax - xmin) / n.toDouble
+    val tdcdf = if (td.size() <= td.getMaxDiscrete()) td.cdfDiscrete(_) else td.cdf(_)
+    (xmin to xmax by step).iterator.map(x => math.abs(tdcdf(x) - cdf(x))).max
+  }
+
+  def gaussianCDF(mean: Double = 0, stdv: Double = 1): CDF = {
+    require(stdv > 0.0)
+    val z = stdv * math.sqrt(2.0)
+    (x: Double) => (1.0 + erf((x - mean) / z)) / 2.0
+  }
+
+  def discreteUniformCDF(xmin: Int, xmax: Int): CDF = {
+    require(xmax > xmin)
+    require(xmin >= 0)
+    val z = (1 + xmax - xmin).toDouble
+    (x: Double) => {
+      if (x < xmin.toDouble) 0.0 else if (x >= xmax.toDouble) 1.0 else {
+        val xi = x.toInt
+        (1 + xi - xmin).toDouble / z
+      }
+    }
+  }
+
+  // https://en.wikipedia.org/wiki/Error_function#Approximation_with_elementary_functions
+  def erf(x: Double): Double = {
+    // erf is an odd function
+    if (x < 0.0) -erf(-x) else {
+      val t = 1.0 / (1.0 + (0.47047 * x))
+      var u = t
+      var s = 0.0
+      s += 0.3480242 * u
+      u *= t
+      s -= 0.0958798 * u
+      u *= t
+      s += 0.7478556 * u
+      s *= math.exp(-(x * x))
+      1.0 - s
+    }
+  }
+}
